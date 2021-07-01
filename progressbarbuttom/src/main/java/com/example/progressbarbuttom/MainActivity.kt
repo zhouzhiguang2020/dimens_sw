d1356f731cf15cdd8f92b6e4482f396ea86fad23
ASUS <596889153@qq.com>
2021-07-01 18:23:29 +0800

    ktx扩展包进阶学习

--- progressbarbuttom/src/main/java/com/example/progressbarbuttom/MainActivity.kt
+++ progressbarbuttom/src/main/java/com/example/progressbarbuttom/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.progressbarbuttom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.razir.progressexample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
